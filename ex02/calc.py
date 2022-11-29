b2fb4afa3deebb6bd9badfdd0a48fe858fcee80f
umiha <c0a211433d@edu.teu.ac.jp>
2022-11-29 16:45:31 +0900

    練習問題7

--- ex02/calc.py
+++ ex02/calc.py
@@ -10,11 +10,14 @@ def button_numclick(event):
     btn = event.widget
     num = btn["text"]
     entry.insert(tk.END,num)
-
+def equal_click(event):
+    eqn=entry.get()
+    resp = eval(eqn)
+    entry.delete(0,tk.END)    
+    entry.insert(tk.END,resp)
 #ウィンドウ作成
 root = tk.Tk()
 root.geometry("300x500")
-
 #4
 entry = tk.Entry(root,width=10,font=("",40),justify="right")
 entry.grid(row=0,column=0,columnspan=3)
@@ -37,8 +40,8 @@ button.grid(column=1,row=4)
 button.bind("<1>",button_numclick)
 
 #=ボタン
-button = tk.Button(root,text="-",font=("",30),width=4,height=2)
+button = tk.Button(root,text="=",font=("",30),width=4,height=2)
 button.grid(column=2,row=4)
-button.bind("<1>",button_numclick)
+button.bind("<1>",equal_click)
 
 root.mainloop()
