8dedcae554bd7ba007cbdcedcad5182fd3c8bd36
umiha <c0a211433d@edu.teu.ac.jp>
2022-11-29 15:57:03 +0900

    練習問題4

--- ex02/calc.py
+++ ex02/calc.py
@@ -5,18 +5,19 @@ def button_click(event):
     btn = event.widget
     num = btn["text"]
     tkm.showinfo("",f"{num}ボタンがクリックされました")
-    
+
 #ウィンドウ作成
 root = tk.Tk()
 root.geometry("300x500")
 
 #4
-#entry = tk.Entry(root,justify="right",width=10,font=("") )
+entry = tk.Entry(root,width=10,font=("",40),justify="right")
+entry.grid(row=0,column=0,columnspan=3)
 
 #ボタン作成
-r,c = 0,0
+r,c = 2,0
 for num in range(9,-1,-1):
-    button = tk.Button(root,text=f"{num}",width=4,height=2,font=("",30))
+    button = tk.Button(root,text=f"{num}",font=("",30),width=4,height=2)
     button.grid(column = c,row = r)
     button.bind("<1>",button_click)
     
@@ -24,6 +25,5 @@ for num in range(9,-1,-1):
     if c%3 == 0:
         r += 1
         c = 0
-
 
 root.mainloop()
